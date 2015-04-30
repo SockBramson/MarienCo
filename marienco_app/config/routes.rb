53b4571c87521e3f654106cbd9279d2f2a10db2b
sockbramson <sock.bramson+github@gmail.com>
2015-04-30 11:00:28 +0000

    Doo

--- marienco_app/config/routes.rb
+++ marienco_app/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'static_pages/blog'
 
   get 'static_pages/services'
+  
+  get 'static_pages/contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- marienco_app/test/controllers/static_pages_controller_test.rb
+++ marienco_app/test/controllers/static_pages_controller_test.rb
@@ -1,28 +1,39 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "MarienCo -- Mobile. Dynamic. Future."
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | MarienCo -- Mobile. Dynamic. Future."
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | MarienCo -- Mobile. Dynamic. Future."
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get blog" do
     get :blog
     assert_response :success
-    assert_select "title", "Blog | MarienCo -- Mobile. Dynamic. Future."
+    assert_select "title", "Blog | #{@base_title}"
   end
 
   test "should get services" do
     get :services
     assert_response :success
-    assert_select "title", "Services | MarienCo -- Mobile. Dynamic. Future."
+    assert_select "title", "Services | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
