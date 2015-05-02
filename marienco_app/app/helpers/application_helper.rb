0392dcc49e95c04a6527f9147459b7634d415f06
sockbramson <sock.bramson+github@gmail.com>
2015-05-02 12:04:33 +0000

    update layout and static pages

--- marienco_app/app/helpers/application_helper.rb
+++ marienco_app/app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "MarienCo -- Synergistic You"
+    base_title = "MarienCo -- Mobile. Dynamic. Future."
     if page_title.empty?
       base_title
     else
